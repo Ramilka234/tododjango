0817eb2253af04c7d9960fde1bd3505f98f8bd2a
ramil mukhtarov <ramilasus@gmail.com>
2023-07-30 21:32:33 +0300

    2

--- tododj/todolist/models.py
+++ tododj/todolist/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    content = models.TextField(blank=True, verbose_name='Текст задачи')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
--- tododj/todolist/views.py
+++ tododj/todolist/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def HomeRender(request):
+    return HttpResponse('<h1>Авторизация</h1>')
